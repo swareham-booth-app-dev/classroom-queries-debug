e13cd8b9c06f4e7a1d18927a5ee0704540bd57e5
Jelani Woods <jelani@firstdraft.com>
2020-10-30 12:48:09 -0500

    Update style of starting code

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,18 @@
 class StudentsController < ApplicationController
   def index
-    @students = Student.all.order({ :created_at => :desc })
+    matching_students = Student.all
+
+    @list_of_students = matching_students.order({ :created_at => :desc })
 
     render({ :template => "students/index.html.erb" })
   end
 
   def show
     the_id = params.fetch("id")
-    @student = Student.where({:id => the_id }).at(0)
+
+    matching_students = Enrollment.where({ :id => the_id })
+
+    @the_student = matching_students.at(0)
 
     render({ :template => "students/show.html.erb" })
   end
